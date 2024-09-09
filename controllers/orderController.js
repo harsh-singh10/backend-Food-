82e69fbc4942187becc1d189f97f569c488084e7
harsh-singh10 <harshgbkumar0139@gmail.com>
2024-09-09 19:22:24 +0530

    complete

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -11,18 +11,66 @@ const placeOrder = async(req,res)=>{
             userId : req.body.userId,
             items:req.body.items,
             amount:req.body.amount,
-            address:req.bode.address
+            address:req.body.address
         })
         
         await newOrder.save();
         await userModel.findByIdAndUpdate(req.body.userId,{cartData:{}});
-
+        res.json({success:true,message:"data is saved in database " })
         
 
     } catch (error) {
+        console.log("Error while order" , error);
+        res.json({success:false,message:"error while ordring food"})
         
     }
 }
 
 
-export {placeOrder}
+// user order for front end 
+
+const userOrder = async (req,res)=>{
+        try {
+                const order = await orderModel.find({userId:req.body.userId});
+                res.json({success:true,data:order})
+        } catch (error) {
+                console.log(error);
+                res.json({success:false,message:"error while fetching user order"})
+                
+        }   
+}
+
+
+// lsitning orders from admin panel 
+
+const listOrder = async (req,res)=>{
+        try {
+
+            const orders = await orderModel.find({});
+            res.json({success:true,data:orders})
+            
+        } catch (error) {
+            console.log(error);
+            res.json({success:false,message:"error while fetching order for admin panel"})
+            
+        }
+}
+
+
+// api for updating status 
+
+const updateStatus = async(req,res)=>{
+
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status})
+        res.json({success:true,message:"Status updated"})
+    } catch (error) {
+        console.log("error while updating status " , error);
+        res.json({success:false, message:"error "})
+        
+    }
+
+}
+
+
+export {placeOrder , userOrder , listOrder , updateStatus}
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,9 +1,15 @@
 import express from "express"
-import { placeOrder } from "../controllers/orderController.js"
+import { listOrder, placeOrder, updateStatus, userOrder } from "../controllers/orderController.js"
 import authMiddleware from "../middleware/auth.middleware.js";
 
 const orderRoute = express.Router();
 
 orderRoute.post("/place" , authMiddleware,placeOrder);
+
+orderRoute.post("/userorder" , authMiddleware,userOrder);
+
+orderRoute.get("/list" , listOrder)
+
+orderRoute.post("/status" , updateStatus)
 
 export default orderRoute
